bdf9ea01dfe2768527d2d66069f7f591c86de2e9
VariableV <student.mhuss@gmail.com>
2020-03-31 22:04:35 -0500

    heroku changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
